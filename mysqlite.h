d0dd5e6fcda159891ae57332a42852cfedd2314b
LC <1771621931@qq.com>
2020-01-09 18:05:09 +0800

    0

---
+++ mysqlite.h
@@ -0,0 +1,32 @@
+#ifndef __MYSQLITE_H
+#define __MYSQLITE_H
+
+#include <sqlite3.h>
+#include "../myinclude/proto.h"
+
+//查询标志位
+uint8_t SELECT_FLAG=0;
+
+//打开数据库
+int open_db(char *path_name, sqlite3 **db);
+
+//创建表
+int creat_table(sqlite3 *db, char *table);
+
+//表中插入数据
+int insert_data(sqlite3 *db, char *table, const DEVICE_DATA * device);
+
+//查询数据   condition查询条件  []=[]
+int select_data(sqlite3 *db, char *table, char *condition, DEVICE_DATA_BUF *device_buf);
+
+//删除表中数据
+int del_data(sqlite3 *db, char *table, char *condition);
+
+//更新数据库数据   condition 更新条件    colum要更新的列        new_date  更新的数值
+int update(sqlite3 *db, char *table, char *condition, char *column, char *new_date);
+
+//关闭数据库
+int close_db(sqlite3 *db);
+
+#endif
+
---
+++ proto.h
@@ -0,0 +1,79 @@
+#ifndef __PROTO_H
+#define __PROTO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sqlite3.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#pragma pack(1)    //指定字节对齐  
+
+#define SERVER_PORT	6666
+#define SERVER_IP	"192.168.154.129"
+
+#define NAME_SIZE   65
+#define ADDR_SIZE   65
+#define PWD_SIZE    33
+#define CNT_SIZE    6
+#define MAX_LEN    1024
+
+/*调试使用*/
+#define DEBUG       1
+
+enum{DEVICE_NAME=0, DEVICE_ID, DEVICE_ADDR, TEMP, HUM, DP_TEMP, DIP_FIRE, SMOG, PM25};
+
+/*
+device_id--2101
+callback--device_addr--02#-101
+callback--temp--24
+callback--hum--66
+callback--dp_temp--18
+callback--dip_fire--0
+callback--smog--200
+callback--PM25--12
+*/
+
+typedef struct regdate
+{
+    char name[NAME_SIZE];
+    char count[CNT_SIZE];
+    char pwd[PWD_SIZE];
+}reg_date_t;
+
+/*设备数据结构*/
+typedef struct device_data
+{
+    char device_name[NAME_SIZE];
+    char device_addr[64];
+    int device_id;
+    uint8_t temp;
+    uint8_t hum;
+    int8_t dp_temp;        //露点温度
+    uint8_t dip_fire;      //浸水—火光开关量  第0位 dip   第1位 fire    1 有信号   0无信号
+    int smog;              //烟雾
+    int PM25;
+}DEVICE_DATA;
+
+/*多个设备数据存储*/
+typedef struct device_data_buf
+{
+    int device_num;
+    DEVICE_DATA *device;
+}DEVICE_DATA_BUF;
+
+#endif
+
+
